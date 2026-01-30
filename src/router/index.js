f3126fb4f7a159f935c88377ed988dc4d66cb386
giacomozoet <giaccommoo@gmail.com>
2026-01-30 10:09:16 +0100

    indexjs router structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { isAuthenticated } from '@/services/authentication.js'
+import HomeView from "@/views/HomeView.vue";
+import WorkspaceView from "@/views/WorkspaceView.vue";
+import LoginView from "@/views/LoginView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {path: '/', component: HomeView, meta: { title: 'Home',  requireAuth: true }},
+    {path: '/workspace', component: WorkspaceView, meta: { title: 'Workspace',  requireAuth: true }},
+    {path: '/login', component: LoginView, meta: { title: 'Login'}},
+  ],
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth) {
+    if (isAuthenticated()) {
+      next() // allowed to access
+    } else {
+      next('/login')
+    }
+  } else {
+    next() // is not a protected routed
+  }
 })
 
 export default router
